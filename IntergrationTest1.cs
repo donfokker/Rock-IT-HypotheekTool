2dc9c851139f4e27a0a6cecd217c89ceb87c9d66
DoniBoye <donfokker@gmail.com>
2024-11-04 12:37:11 +0100

    update

--- IntergrationTest1.cs
+++ IntergrationTest1.cs
@@ -2,7 +2,7 @@ using HypotheekTool.Classes;
 
 namespace Rock_IT_HypotheekTool;
 
-public class UnitTest1
+public class IntergrationTest
 {
     //Here resides the test data of the project
     string yearlyIncomeResult = "10000";
@@ -17,9 +17,14 @@ public class UnitTest1
     string[] allowedInterestYears = ["1", "5", "10", "20", "30"];
 
     [Fact]
-    public void YearlyIncomeTest()
+    public void InputTests()
     {
+        string postalCode = null;
+        int? amountOfYears = null;
         int? yearlyIncome = null;
+        bool? hasStudentDebt = null;
+
+        //Get user data for calculations
         if (yearlyIncome == null)
         {
             if (int.TryParse(yearlyIncomeResult, out int yearlyIncomeToInt))
@@ -32,11 +37,7 @@ public class UnitTest1
                 Assert.Fail("YearlyIncome input is invalid");
             }
         }
-    }
 
-    [Fact]
-    public void StudentDebtTest(){
-        bool? hasStudentDebt = null;
         if (hasStudentDebt == null)
         {
             if (studentDebtResult is "Yes" or "yes")
@@ -54,15 +55,11 @@ public class UnitTest1
                 Assert.Fail("HasStudentDebt input is invalid");
             }
         }
-    }
 
-    [Fact]
-    public void ValidYearsTest()
-    {
-        int? amountOfYears = null;
         if (amountOfYears == null)
         {
-            if (int.TryParse(yearsOfInterestResult, out int yearsOfInterest) && allowedInterestYears.Contains(yearsOfInterestResult))
+            if (int.TryParse(yearsOfInterestResult, out int yearsOfInterest) &&
+                allowedInterestYears.Contains(yearsOfInterestResult))
             {
                 amountOfYears = yearsOfInterest;
                 Assert.Equal(yearsOfInterestResult, amountOfYears.ToString());
@@ -73,13 +70,7 @@ public class UnitTest1
                 Assert.Fail("YearsOfInterest input is invalid");
             }
         }
-    }
 
-    //TODO: code only checks blacklisted postalcodes non-existing postal codes still pass despite being invalid.
-    [Fact]
-    public void PostalCodeTest()
-    {
-        string postalCode = null;
         if (postalCode == null)
         {
             postalCode = postalCodeResult;
